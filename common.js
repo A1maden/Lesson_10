0a442f714f95e63d0be7933cdaff5555f2d4f843
A1maden <almaden87@gamil.com>
2015-07-12 21:08:37 +0300

    1

--- common.js
+++
@@ -1,9 +0,0 @@
-$(".row1_zoom").click(function () 
-{
-    if ($(".gallery_row1-bigimg").is(":visible")) {
-        $(".gallery_row1-bigimg").fadeOut(600);
-    }
-    else {
-        $(".gallery_row1-bigimg").fadeIn(600);
-    };
-});
